280bc92cefe95e65aaa7a5fa7f1d8eca12ceb43a
Gayathri <gayathrisanthoshpc3008@gmail.com>
2026-08-03 12:32:09 +0530

    Added smart memory update system

--- backend/agents/memory_agent.py
+++ backend/agents/memory_agent.py
@@ -6,10 +6,26 @@ def save_memory(case_data, file_path="memory/case_memory.json"):
     with open(file_path, "r") as file:
         memory = json.load(file)
 
-    memory["cases"].append(case_data)
+
+    case_exists = False
+
+
+    for index, case in enumerate(memory["cases"]):
+
+        if case["case_id"] == case_data["case_id"]:
+
+            memory["cases"][index] = case_data
+            case_exists = True
+            break
+
+
+    if not case_exists:
+        memory["cases"].append(case_data)
+
 
     with open(file_path, "w") as file:
         json.dump(memory, file, indent=4)
+
 
 
 def get_memory(file_path="memory/case_memory.json"):
